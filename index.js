753c0158180540534c9ed394bcd79c62657ce5c9
Kyaw Zin Tun <kyawzintun@kyawzintun.local>
2017-11-09 16:15:35 +0630

    change port listen

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/timestamp-ms', function (req, res){
   res.json(timestamp);
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Node app is running on port', process.env.PORT || 5000);
 });
